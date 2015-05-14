d8574c33b6e15f5841fc5fd92348298e620e214d
literallybenjam <bjmns@me.com>
2015-05-14 07:00:32 -0700

    fixed index--back to zero, yo

--- annotation.js
+++ annotation.js
@@ -16,17 +16,17 @@ Annotation.parse = function (element) {
     var i;
     var a;
     var annotation;
-    for (i = 1; i <= element.getElementsByClassName("annotation").length; i++) {
+    for (i = 0; i < element.getElementsByClassName("annotation").length; i++) {
         annotation = element.getElementsByClassName("annotation").item(i);
         if (annotation.previousElementSibling && annotation.previousElementSibling.tagName.toUpperCase() === "A" && annotation.previousElementSibling.classList.contains("annotation-link")) continue;
         a = document.createElement("A");
-        a.appendChild(document.createElement("SUP")).textContent = i;
-        a.href = "#annotation-link-" + i;
+        a.appendChild(document.createElement("SUP")).textContent = (i + 1);
+        a.href = "#annotation-link-" + (i + 1);
         a.className = "annotation-link";
         a.addEventListener("click", Annotation, true);
-        element.getElementsByClassName("annotation").item(i - 1).parentNode.insertBefore(a, element.getElementsByClassName("annotation").item(i - 1));
-        element.getElementsByClassName("annotation").item(i - 1).parentNode.insertBefore(document.createTextNode(" "), element.getElementsByClassName("annotation").item(i - 1));
-        element.getElementsByClassName("annotation").item(i - 1).setAttribute("hidden", "");
+        element.getElementsByClassName("annotation").item(i).parentNode.insertBefore(a, element.getElementsByClassName("annotation").item(i));
+        element.getElementsByClassName("annotation").item(i).parentNode.insertBefore(document.createTextNode(" "), element.getElementsByClassName("annotation").item(i));
+        element.getElementsByClassName("annotation").item(i).setAttribute("hidden", "");
     }
     return element;
 }
